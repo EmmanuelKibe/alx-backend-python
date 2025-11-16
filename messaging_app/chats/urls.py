496bc187e503c6ccaa3ce4917139b3261b6197b0
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-16 20:27:16 +0300

    add URL routing for Conversation and Message views

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include  
+from rest_framework import routers
+from .views import ConversationViewSet, MessageViewSet
+
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversations')
+router.register(r'messages', MessageViewSet, basename='messages')
+
+urlpatterns = [
+    path('', include(router.urls)),  
+]
