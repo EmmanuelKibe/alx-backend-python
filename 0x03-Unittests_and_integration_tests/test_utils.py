d75ae57a3e414447ac0fad4782706cac67a3f511
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-20 18:03:07 +0300

    Increased the spacing between test cases from 1 to 2 to meet pycodestyle standards

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,6 +26,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
         self.assertEqual(str(cm.exception), repr(path[-1]))
 
+
 class TestGetJson(unittest.TestCase):
 
     @parameterized.expand([
@@ -42,6 +43,7 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
 
 class TestMemoize(unittest.TestCase):
 
