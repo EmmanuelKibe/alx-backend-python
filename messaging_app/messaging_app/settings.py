b97a1f4e23e176f5ec15c721099d0c46f44822d3
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-26 08:03:14 +0300

    Configured the settings to allow JWT authentication

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'chats',
 ]
 
@@ -47,7 +48,7 @@ REST_FRAMEWORK = {
     ],
 
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.JWTAuthentication",
     ],
 }
 
