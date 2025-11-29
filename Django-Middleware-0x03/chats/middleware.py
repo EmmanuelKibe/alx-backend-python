87d031bd6e224277bb68dc8828ebfa04647514bb
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-29 16:06:18 +0300

    Restrict access to the app during hours outside 9pm and 6am

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -23,3 +23,20 @@ class RequestLoggingMiddleware:
         response = self.get_response(request)
 
         return response
+    
+class RestrictAccessByTimeMiddleware:
+    """
+    Middleware that restricts access outside 9pm and 6am.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        if current_hour < 6 or current_hour >= 21:
+            from django.http import HttpResponseForbidden
+            return HttpResponseForbidden("Access is restricted between 9 PM and 6 AM.")
+
+        response = self.get_response(request)
+        return response
