c1d72c2fb05aaabe57b9abafefca2086b58f7941
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-30 18:17:12 +0300

    Include a parent_message field to represent replies

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -13,7 +13,9 @@ class Message(models.Model):
     receiver = models.CharField(max_length=100)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    edited_by = models.BooleanField(default=False) 
+    edited = models.BooleanField(default=False) 
+    parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
+    edited_by = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.timestamp}"
