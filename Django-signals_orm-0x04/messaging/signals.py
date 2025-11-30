2ed9ff40f3d32524a62584af421e3233f1051e62
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-30 17:34:14 +0300

    Use pre_save instead of post_save so as to save content before overwriting

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
 #Create Django signals to trigger a notification when a new Message instance is created
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from .models import Message, Notification, User, MessageHistory
 
@@ -18,7 +18,7 @@ def notify_new_message(sender, instance, created, **kwargs):
         except User.DoesNotExist:
             print(f"Receiver user {instance.receiver} does not exist. No notification created.")
 
-@receiver(post_save, sender=Message)
+@receiver(pre_save, sender=Message)
 def log_message_edit(sender, instance, created, **kwargs):
     if not created and instance.edited:
         old_message = Message.objects.get(id=instance.id)
