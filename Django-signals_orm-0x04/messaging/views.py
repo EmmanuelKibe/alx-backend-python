438464ae1921fa8f39652e24ababccd6530486af
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-30 18:24:32 +0300

    Optimize querying for messages and their replies

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,6 +1,6 @@
 #Create views here.
 from django.shortcuts import render
-from .models import User
+from .models import User, Message
 from django.http import HttpResponse
 
 #Allow users to delete their account
@@ -11,3 +11,15 @@ def delete_user(request, username):
         return HttpResponse(f"User {username} deleted successfully.")
     except User.DoesNotExist:
         return HttpResponse(f"User {username} does not exist.", status=404)
+    
+#Optimize querying for messages and their replies
+def view_message_with_replies(request, message_id):
+    try:
+        message = Message.objects.prefetch_related('replies').get(id=message_id)
+        replies = message.replies.all()
+        response_content = f"Message from {message.sender} to {message.receiver}: {message.content}\nReplies:\n"
+        for reply in replies:
+            response_content += f"- {reply.sender} to {reply.receiver}: {reply.content}\n"
+        return HttpResponse(response_content)
+    except Message.DoesNotExist:
+        return HttpResponse(f"Message with ID {message_id} does not exist.", status=404)
