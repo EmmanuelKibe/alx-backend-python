e7b06cac2cf90536047b09756cba1b0de95e4617
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-30 17:19:30 +0300

    Created a signal that listens for new messages and creates a notification for receivers

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -2,11 +2,19 @@
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Message
+from .models import Message, Notification, User
 
 
 @receiver(post_save, sender=Message)
 def notify_new_message(sender, instance, created, **kwargs):
     if created:
         print(f"New message created from {instance.sender} to {instance.receiver}: {instance.content}")
+
+#Create a signal that listens for new messages and creates a notification for receivers
+        try:
+            receiver_user = User.objects.get(username=instance.receiver)
+            Notification.objects.create(user=receiver_user, message=instance)
+            print(f"Notification created for user {receiver_user.username} about new message ID {instance.id}")
+        except User.DoesNotExist:
+            print(f"Receiver user {instance.receiver} does not exist. No notification created.")
 
