6d7211bc7e2d6cd8c3adf0e615687a9fd81f0967
EmmanuelKibe <emmanuelkibe148@gmail.com>
2025-12-16 16:19:35 +0300

    Synced changes made in docker-compose.yml to settings.py to avoid crashing of the containers

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -102,7 +102,7 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE', 'messaging_db'),
+        'NAME': os.environ.get('MYSQL_DB', 'messaging_db'),
         'USER': os.environ.get('MYSQL_USER', 'messaging_user'),
         'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'messaging_pass'),
         'HOST': os.environ.get('MYSQL_HOST', 'db'),
